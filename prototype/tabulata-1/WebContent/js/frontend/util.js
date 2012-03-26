867291d0f9263a28cdc713a104b6594f204e15b8
Samuel Rutishauser <samuel@rutishauser.name>
2012-03-26 11:38:43 -0400

    split main.js

---
+++ prototype/tabulata-1/WebContent/js/frontend/util.js
@@ -0,0 +1,32 @@
+
+function html() {
+	
+}
+
+html.td = function(elem) {
+	return html.crelem("td", elem);
+};
+
+html.tr = function(elem) {
+	return html.crelem("tr", elem);
+};
+
+html.th = function(elem) {
+	return html.crelem("th", elem);
+};
+
+
+html.input = function (id, className, value) {
+	var e = document.createElement("input");
+	e.className = className;
+	e.id = id;
+	e.value = value;
+	return e;
+};
+
+html.crelem = function (name, inside) {
+	var elem = document.createElement(name);
+	if (inside != undefined)
+		$(elem).append(inside);
+	return elem;	
+};
