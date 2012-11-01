cc125c4a8158d9cd054047f1e9a138a86c079ff3
Samuel Rutishauser <samuel@rutishauser.name>
2012-11-01 10:24:40 +0100

    allow data to be fetched from other servers

--- prototype/app.js
+++ prototype/app.js
@@ -40,16 +40,20 @@ db.on("error", function (err) {
 });
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(function test (req, res, next){
+        res.header('Access-Control-Allow-Origin', '*');
+        next();
+    });
+    app.use(app.router);
+    app.use(require('stylus').middleware(__dirname + '/public'));
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
